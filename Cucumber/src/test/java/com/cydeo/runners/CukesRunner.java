20e58ce5db0c400ebdda57b4b573cf2f30f4a67c
Can-Ali <51790329+Can-Ali@users.noreply.github.com>
2022-07-24 12:24:44 +0100

    Cucumber repo added

---
+++ Cucumber/src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitons",
+        dryRun = false, // true means that do not run my step definitions but only check if any step is missing snippet.
+        tags = "@scenarioOutline"
+)
+
+public class CukesRunner {
+
+}
